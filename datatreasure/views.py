8e65e9a2b54c761f7750934ab0fa2b6c1ee43ba6
Ritwik Gupta <ritwikg2004@live.com>
2015-04-11 13:47:47 -0400

    Server retrieves hashtags

--- datatreasure/views.py
+++ datatreasure/views.py
@@ -1,6 +1,9 @@
 from flask import render_template, url_for, redirect, flash, send_from_directory, request
 from datatreasure import datatreasure
+from keys import one, two, three, four
 import os
+import tweepy
+import re
 import json
 
 @datatreasure.route('/')
@@ -13,4 +16,26 @@ def index():
 @datatreasure.route('/search', methods=['POST'])
 
 def search():
-  keyword = request.form['keyword']
+    keyword = request.form['keyword']
+
+    auth = tweepy.OAuthHandler(one, two)
+    auth.set_access_token(three, four)
+
+    api = tweepy.API(auth)
+
+    hashtag = list()
+
+    for tweet in tweepy.Cursor(api.search, q=keyword, count=100, lang="en").items(100):
+    	temp = re.findall(r'#\w+', tweet.text)
+    	if temp:
+    		for i in temp:
+    			hashtag.append(i)
+    	else:
+    		pass
+
+    print hashtag
+    str = ""
+    for i in hashtag:
+        str += i + " "
+
+    return str
