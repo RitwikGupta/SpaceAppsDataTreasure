939024e42984d9ea10b64b166137dc55f607f354
andrew <anm898989@gmail.com>
2015-04-11 13:40:38 -0400

    adds keyword seach ui and routing

--- datatreasure/views.py
+++ datatreasure/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, url_for, redirect, flash, send_from_directory
+from flask import render_template, url_for, redirect, flash, send_from_directory, request
 from datatreasure import datatreasure
 import os
 import json
@@ -6,5 +6,11 @@ import json
 @datatreasure.route('/')
 @datatreasure.route('/home')
 @datatreasure.route('/home/')
+
 def index():
-    return "Hi!"
+    return render_template("home.html")
+
+@datatreasure.route('/search', methods=['POST'])
+
+def search():
+  keyword = request.form['keyword']
